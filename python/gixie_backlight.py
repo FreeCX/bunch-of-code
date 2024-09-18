a5daece3046faf9fd6d7eddc38032e16f676eaf1
Alexey Golubev <dr.freecx@gmail.com>
2024-09-18 10:09:50 +0300

    chore: restruct

--- python/gixie_backlight.py
+++ python/gixie_backlight.py
@@ -8,13 +8,20 @@ import websockets
 async def backlight_set(ip, value):
     assert not (value < 1 or value > 255)
 
+    # set
     msg = {"cmdType": 1, "cmdNum": 14, "cmdCtx": {"value": value}}
+    # get
+    # msg = {"cmdType": 0, "cmdNum": 14}
     async with websockets.connect(f"ws://{ip}:81") as ws:
-        await ws.send(json.dumps(msg))
+        # recv Connected
         await ws.recv()
+        # send data
+        await ws.send(json.dumps(msg))
+        # recv Response
+        print(await ws.recv())
 
 
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser(description="Gixie clock brightness")
     parser.add_argument("-i", dest="ip", type=str, default=None, help="gixie clock ip address")
     parser.add_argument("-b", dest="backlight", type=int, default=None, help="backlight value (1-255)")
@@ -22,5 +29,10 @@ if __name__ == "__main__":
 
     if args.backlight:
         asyncio.run(backlight_set(args.ip, args.backlight))
-    else:
-        parser.print_help()
+        return
+
+    parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
